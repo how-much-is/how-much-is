7bf53f63282de2f89e566d296dffe025c4025ed6
KSJ <ksjj0911@gmail.com>
2026-04-08 17:26:39 +0900

    사용자 정보 변경 페이지 commit

--- how-much-is/src/App.vue
+++ how-much-is/src/App.vue
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
-<script setup>
-import userSetting from './pages/userSettings.vue';
-</script>
-
 <template>
   <div>
-    <userSetting></userSetting>
+    <router-link to="/">Home</router-link>
+    <br />
+    <router-link to="/monthly">Monthly</router-link>
+
+    <router-view></router-view>
   </div>
-=======
-<template>
- <div>
-
-  <router-link to="/">Home</router-link>
-  <br>
-  <router-link to="/monthly">Monthly</router-link>
-
-  <router-view></router-view>
- </div>
->>>>>>> 00be46bf0e8ce54fbd2c6a6a2b36b1e0049298c6
 </template>
 
 <script setup>
 import MonthlyList from './components/monthly/MonthlyList.vue';
-
-
-
 </script>
-
-
 
 <style scoped></style>
