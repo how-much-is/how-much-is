1bcacb29a47107bc0835b883120a389fa293778e
KSJ <ksjj0911@gmail.com>
2026-04-09 14:43:19 +0900

    settings 사용자 정보 수정 db 연결

--- how-much-is/src/App.vue
+++ how-much-is/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link>
+    <!-- <router-link to="/">Home</router-link>
     <br />
     <router-link to="/monthly">Monthly</router-link>
 
-    <router-view></router-view>
+    <router-view></router-view> -->
+    <!-- <Category></Category> -->
+    <Settings></Settings>
   </div>
 </template>
 
 <script setup>
-import MonthlyList from './components/monthly/MonthlyList.vue';
+// import MonthlyList from './components/monthly/MonthlyList.vue';
+// import Category from './pages/Category.vue';
+import Settings from './pages/Settings.vue';
 </script>
 
 <style scoped></style>
